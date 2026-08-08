2cc0062f155e0e3569687e03125e209f968253c8
Pharoah Egbuna <pharoahegbuna@gmail.com>
2026-08-08 17:36:23 -0400

    Final Lesson 6 Steps

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,17 @@ function App() {
     setTodoList((previous) => previous.map(todo => todo.id === id ? {...todo, isCompleted: true} : todo));
   }
 
+  function updateTodo(editedTodo) {
+    const updatedTodos = todoList.map(todo => todo.id === editedTodo.id ? {...todo, title: editedTodo.title} : todo);
+    setTodoList(updatedTodos);
+
+  }
+
   return (
     <div>
       <h1>Todo List</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} onCompleteTodo = {completeTodo} />
+      <TodoList todoList={todoList} onCompleteTodo = {completeTodo} onUpdateTodo = {updateTodo} />
     </div>
   );
 }
--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,14 +1,40 @@
 import TextInputWithLabel from '/src/shared/TextInputWithLabel.jsx';
 import { useState } from 'react';
 
-function TodoListItem({todo, onCompleteTodo}) {
+function TodoListItem({todo, onCompleteTodo, onUpdateTodo}) {
 
     const [isEditing, setIsEditing] = useState(false);
+    const [workingTitle, setWorkingTitle] = useState(todo.title);
+
+    function handleCancel() {
+        setWorkingTitle(todo.title);
+        setIsEditing(false);
+    }
+
+    function handleEdit(event) {
+        setWorkingTitle(event.target.value)
+    }
+
+    function handleUpdate(event) {
+        event.preventDefault();
+        if (!isEditing) {
+            return;
+        }
+        onUpdateTodo({...todo, title: workingTitle});
+        setIsEditing(false);
+    }
 
     return (
     <li>
-        <form>
-            {isEditing ? ( <TextInputWithLabel value={todo.title}/>) : (
+        <form onSubmit={handleUpdate}>
+            {isEditing ? (
+                <>
+                <TextInputWithLabel value={workingTitle} onChange={handleEdit}/>
+                <button type="button" onClick={handleCancel}>Cancel</button>
+                <button type="button" onClick={handleUpdate}>Update</button>
+                <TextInputWithLabel value={workingTitle} onChange={handleEdit}/>
+                </> 
+            ) : (
                 <>
                 <label>
                     <input 
