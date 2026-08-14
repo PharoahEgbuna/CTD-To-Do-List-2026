a68b613f697be6fb0092bc2515bd5dd8816d7434
Pharoah Egbuna <pharoahegbuna@gmail.com>
2026-08-14 17:55:37 -0400

    Airhub edits #3

--- src/features/TodoForm.jsx
+++ src/features/TodoForm.jsx
@@ -11,7 +11,7 @@ function TodoForm({ onAddTodo }) {
     const handleAddTodo = (event) => { 
         event.preventDefault();
 
-        if (workingTodoTitle) {
+        if (isValidTodoTitle(workingTodoTitle)) {
             onAddTodo(workingTodoTitle);
             setWorkingTodoTitle('');
         }
