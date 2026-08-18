3ba21ccde9c6e11da02aea54a25a490cd0dcaebb
Pharoah Egbuna <pharoahegbuna@gmail.com>
2026-08-18 13:08:39 -0400

    Edited App.jsx & Logon.jsx to better match intructions.

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ function App() {
   const [token, setToken] = useState('');
 
   return (
-    <>
+    <div>
     <Header token={token} onSetToken={setToken} onSetEmail={setEmail}/>
     {token ? <TodosPage token={token}/> : <Logon onSetEmail={setEmail} onSetToken={setToken} />}
-    </>
+    </div>
   );
 }
 
--- src/features/Logon.jsx
+++ src/features/Logon.jsx
@@ -4,19 +4,17 @@ export default function Logon({onSetEmail, onSetToken}) {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [authError, setAuthError] = useState(false);
+    const [authError, setAuthError] = useState('');
     const [isLoggingOn, setIsLoggingOn] = useState(false);
 
     async function handleSubmit(event) {
+        event.preventDefault();
+        setIsLoggingOn(true);
         try {
-            event.preventDefault();
-            setIsLoggingOn(true);
-            const response = await fetch('api/users/logon/', {
+            const response = await fetch(`/api/users/logon/`, {
                 method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'credentials': 'include',
-                },
+                headers: { 'Content-Type': 'application/json' },
+                credentials: 'include',
                 body: JSON.stringify({email, password})
             });
 
@@ -37,22 +35,27 @@ export default function Logon({onSetEmail, onSetToken}) {
     }
 
     return (
-        <form >
+        <form>
             {authError && <p>{authError}</p>}
-            <input
-                htmlFor="email"
+            <label htmlFor='email'>
+                <input
+                id = "email"
                 placeholder="Enter email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 required
-            />
-            <input
+                />
+            </label>
+            <label>
+                <input
                 htmlFor="password"
+                id = "password"
                 placeholder="Enter password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
-            />
+                />
+            </label>
             <button type="submit" onClick={handleSubmit} disabled={isLoggingOn}>
                 {isLoggingOn ? 'Logging in...' : 'Log In'}
             </button>
