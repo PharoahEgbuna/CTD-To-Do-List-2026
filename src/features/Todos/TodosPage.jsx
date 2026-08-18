83a00371382169645f10af33b505c79fbc6e2f55
Pharoah Egbuna <pharoahegbuna@gmail.com>
2026-08-17 20:08:54 -0400

    Step 6 Complete

--- src/features/Todos/TodosPage.jsx
+++ src/features/Todos/TodosPage.jsx
@@ -12,21 +12,20 @@ export default function TodosPage({ token }) {
         async function fetchTodos() {
             try {
                 setIsTodoListLoading(true);
-                const response = await fetch('api/tasks', 
-                    { headers: {
+                const response = await fetch('/api/tasks', {
+                    headers: {
                         'X-CSRF-TOKEN' : token,
                     },
-                    credentials: 'include',
+                    credentials: 'include'
                 });
 
-                if (response.status === 200) {
+                if (response.ok) {
                     const data = response.json()
                     setTodoList(data)
                 } else if (response.status === 401) {
                     throw new Error('Unauthorized')
                 } else if (!response.ok) {
                     throw new Error('Something went wrong.')
-
                 }
             } catch (appError) {
                 setError(appError)
@@ -41,7 +40,7 @@ export default function TodosPage({ token }) {
     }, [token])
 
     
-    function addTodo(todoTitle) {
+    async function addTodo(todoTitle) {
         let newTodo = {
         id: Date.now(),
         title: todoTitle, 
@@ -49,20 +48,87 @@ export default function TodosPage({ token }) {
         };
 
         setTodoList((previous) => [newTodo, ...previous]);
+
+        const response = await fetch('/api/tasks', {
+                    method: 'POST',
+                    headers: {
+                        'X-CSRF-TOKEN' : token,
+                        'Content-Type': 'application/json'
+                    },
+                    credentials: 'include',
+                    body: JSON.stringify({ title: newTodo.title, isCompleted : newTodo.isCompleted})
+        });
+
+        if (response.ok) {
+            const data = await response.json;
+            setTodoList([{data, id:Date.now()} ,...setTodoList.slice(1)])
+        } else {
+            setTodoList(previous => previous)
+            setError('Failed to add Todo')
+        }
     }
 
-    function completeTodo(id) {
+    async function completeTodo(id) {
+        
         setTodoList((previous) => previous.map(todo => todo.id === id ? {...todo, isCompleted: true} : todo));
+
+        try {
+
+            const response = await fetch(`/api/tasks/${id}`,{
+                method: 'PATCH',
+                headers: {
+                    'X-CSRF-TOKEN' : token,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({isCompleted: true})
+            })
+
+            if (!response.ok) {
+                throw new Error('Failed to complete Todo')
+            }
+        } catch {
+            setTodoList(previous => previous)
+        }
     }
 
-    function updateTodo(editedTodo) {
+    async function updateTodo(editedTodo) {
         const updatedTodos = todoList.map(todo => todo.id === editedTodo.id ? {...editedTodo} : todo);
+
         setTodoList(updatedTodos);
+
+        try {
+
+            const response = await fetch(`/api/tasks/${editedTodo.id}`,{
+                method: 'PATCH',
+                headers: {
+                    'X-CSRF-TOKEN' : token,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({title: editedTodo.title, isCompleted: editedTodo.isCompleted})
+            })
+
+            if (!response.ok) {
+                throw new Error('Failed to complete Todo')
+            }
+        } catch (e) {
+            setTodoList(previous => previous)
+            setError(e);
+        }
     }
 
     return (
     <div>
+      { error ? (
+        <>
+        <p>{`${error}`}</p> 
+        <button onClick={setError('')}>Clear Error</button>
+        </>) : null 
+      }
+
+      { isTodoListLoading ? (<p>{`Loading...`}</p> ) : null }
+
       <TodoForm onAddTodo={addTodo} />
+
       <TodoList todoList={todoList} onCompleteTodo = {completeTodo} onUpdateTodo = {updateTodo} />
     </div>
   );
