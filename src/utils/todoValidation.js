55afa8420f84b37b2c42a9a9db2ca2a790e20638
Pharoah Egbuna <pharoahegbuna@gmail.com>
2026-09-14 07:34:26 -0400

    Extended allowable todo length

--- src/utils/todoValidation.js
+++ src/utils/todoValidation.js
@@ -1,8 +1,8 @@
 export function isValidTodoTitle( title ) {
   let trimmedTitle = title.trim();
 
-  //Ensure the todo length is greater than 0, less than 20, and not entirely composed of numbers. 
-  if (trimmedTitle.length > 0 && trimmedTitle.length <= 20 && isNaN(trimmedTitle)) { return true }
+  //Ensure the todo length is greater than 0, less than 50, and not entirely composed of numbers. 
+  if (trimmedTitle.length > 0 && trimmedTitle.length <= 50 && isNaN(trimmedTitle)) { return true }
 
   return false;
 }
