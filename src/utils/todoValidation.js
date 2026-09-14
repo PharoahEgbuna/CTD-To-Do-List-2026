c01172c0944be182fe32e438ec80597da8ee7eca
Pharoah Egbuna <pharoahegbuna@gmail.com>
2026-09-13 21:55:32 -0400

    Increased Todo validation check before adding

--- src/utils/todoValidation.js
+++ src/utils/todoValidation.js
@@ -1,3 +1,8 @@
 export function isValidTodoTitle( title ) {
-  return title.trim() !== '';
+  let trimmedTitle = title.trim();
+
+  //Ensure the todo length is greater than 0, less than 20, and not entirely composed of numbers. 
+  if (trimmedTitle.length > 0 && trimmedTitle.length <= 20 && isNaN(trimmedTitle)) { return true }
+
+  return false;
 }
