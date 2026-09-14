27c2a7b1ca87bf53fe52479c9891fa58b75582e1
Pharoah Egbuna <pharoahegbuna@gmail.com>
2026-09-13 21:10:45 -0400

    Moved context initialization to another file to clear errors.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { AuthProvider } from './contexts/AuthContext.jsx'
+import { AuthProvider } from './contexts/AuthProvider.jsx'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router';
