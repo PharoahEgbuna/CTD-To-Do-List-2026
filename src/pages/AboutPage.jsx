679a7242be24559a83a45324a31b5ac7c346843e
Pharoah Egbuna <pharoahegbuna@gmail.com>
2026-09-14 07:41:35 -0400

    Updated page description

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,8 +4,8 @@ export default function AboutPage() {
             <h1>About:</h1>
             <p>Here I've created a todo list application using Javascript, Vite, React, and React-Router.</p>
             <p>This application allows users to sign in and manage their list of tasks.</p>
-            <p>Tasks can be added, deleted, renamed, and marked as complete.</p>
-            <p>In addition, tasks can be filtered using the search bar, and sorted by creation date and alphabetically.</p>
+            <p>Tasks can be added, deleted, renamed, and marked/unmarked as complete.</p>
+            <p>In addition, tasks can be filtered using the search bar or sorted by creation date, alphabetically, or by completion status.</p>
         </div>
     );
 }
